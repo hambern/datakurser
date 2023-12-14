13cfa76cff853451fb25f839cf412d28b50b7fe4
Mattias Hamberg <mattias@hambergmedia.se>
2023-12-14 12:51:19 +0100

    Merge

--- webbserverprogrammering_1/projekt/projekt_chat/index.php
+++ webbserverprogrammering_1/projekt/projekt_chat/index.php
@@ -13,6 +13,10 @@ if ($conn->connect_error) {
     die("Anslutning misslyckades: " . $conn->connect_error);
 } 
 
+$sql = "SELECT * FROM posts p JOIN users u ON p.user_id = u.id ORDER BY p.created_at DESC";
+$result = $conn->query($sql);
+
+
 // Det är en bra princip att här uppe göra bort all php så att man sedan kan använda html-delen nedanför enbart till att visa upp variablerna. Idealet är att det ska vara enkelt, om någon vill ha datan i ett annat format än HTML, enkelt kan byta ut nedanstående del utan att några viktiga funktioner drabbas.
 ?>
 <!DOCTYPE html>
@@ -25,5 +29,14 @@ if ($conn->connect_error) {
 <body>
     <h1>Student-chatten</h1>
     <p>Du är ansluten till databasen "<?= $dbname ?>" på "<?= $servername ?>" med användaren "<?= $username ?>".</p>
+
+    <?php while($row = $result->fetch_assoc()) : ?>
+
+        <code><pre>
+        <?= print_r($row, true) ?>
+        </pre></code>
+
+    <?php endwhile ?>
+
 </body>
 </html>
